d31b5f49104ff734fab4e9472349beb9f205dd36
Thais Melim <thaiscristinarisonho@gmail.com>
2022-11-16 15:33:38 -0300

    Implementação Dark Mode, criando hooks e UseEffect e Supabase

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
+import DarkModeSwitch from "./components/DarkModeSwitch";
 import Search from "./components/Search";
-
 
 const StyledMenu = styled.header`
   display: flex;
@@ -33,6 +33,7 @@ export default function Menu({ valorDoFiltro, setValorDoFiltro }) {
         <Logo />
       </div>
       <Search valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+      <DarkModeSwitch />
     </StyledMenu>
   );
 }
